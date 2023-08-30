648af44ceead89f73614f0703803126282a53442
parker-harrelson <pharrelson@harding.edu>
2023-08-29 22:01:53 -0500

    just adding some variables to get myself started on the baseline for the calculation

--- MoonPhaseCal.py
+++ MoonPhaseCal.py
@@ -1,3 +1,9 @@
 
-just_some_var = 56.3
-print(just_some_var)
+cynodic_phase_time = 29.530588
+
+base_new_moon_mon = 9
+base_new_moon_day = 12
+base_new_moon_year = 2023
+base_new_moon_hour = 8
+base_new_moon_minute = 39
+
